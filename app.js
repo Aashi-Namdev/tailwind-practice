7a2bdcf3d0ba49a7598862e31fdd299aebceb17b
Aashi-Namdev <ashishnamdev67370@gmail.com>
2025-10-22 15:47:33 +0530

    Initial commit with navbar and hero section

---
+++ app.js
@@ -0,0 +1,5 @@
+let navDialogue = document.querySelector("#nav-dialogue");
+let menuBar = document.querySelector("#navbar > #navbar-r")
+function handleMenu(){
+    navDialogue.classList.toggle("hidden");
+}
